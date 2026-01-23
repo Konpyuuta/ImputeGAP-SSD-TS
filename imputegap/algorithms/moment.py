0abce98a8d6e2c56b5ccd157dbe4ed317d18c0e2
Konpyuuta <maurice.amon@outlook.com>
2026-01-23 15:33:44 +0100

    integrated MOMENT

--- imputegap/algorithms/moment.py
+++ imputegap/algorithms/moment.py
@@ -1,4 +1,7 @@
 import numpy as np
+import torch
+
+from imputegap.wrapper.AlgoPython.MOMENT.MOMENTImputer import to_moment_imputation_format, MOMENTImputer
 
 
 def moment_impute(incomp_data, params=None):
@@ -33,9 +36,30 @@ def moment_impute(incomp_data, params=None):
     """
 
     # core of the algorithm
-    mean_value = np.nanmean(incomp_data)
 
-    # Imputation
-    recov_data = np.nan_to_num(incomp_data, nan=mean_value)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device:", device)
+
+    data = np.array([
+        [5, 2, np.nan, 4, 1, 7],
+        [3, np.nan, 6, 8, 2, 9],
+    ], dtype=np.float32)
+    print(data.shape)
+    data = np.tile(data, (1, 300))  # (2, 1800)
+
+    print(incomp_data.shape)
+    print(incomp_data)
+    x_enc, input_mask = to_moment_imputation_format(
+        incomp_data,
+        window_size=incomp_data.shape[1],
+        stride=incomp_data.shape[1],
+        channels_first=True
+    )
+
+    x_enc = x_enc.to(device).float()
+    input_mask = input_mask.to(device).long()
+    momentimputer = MOMENTImputer()
+    momentimputer.init_dataset()
+    recov_data = momentimputer.impute(x_enc, input_mask)
 
     return recov_data
